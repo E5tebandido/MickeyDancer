b090e19c94ec7e62422184d75cca76d424fc8d62
Andrés Esteban Posada Rincón <44687875+Teban18@users.noreply.github.com>
2020-04-21 14:52:50 -0500

    First Version using native JavaScript, css and html

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function cambiarClase(){
+    let siteNav = document.getElementById('sitenav');
+    siteNav.classList.toggle('site-nav-open');
+    let menuOpen = document.getElementById('menutoggle');
+    menuOpen.classList.toggle('menu-open');
+}
