d64f89a51702e69af9b1e2f8291e9b20b01f692c
Esteban Posada <aepr_18_@hotmail.com>
2020-05-29 21:07:49 -0500

    Imported fbx model got by mixamo

--- js/app.js
+++
@@ -1,6 +0,0 @@
-function cambiarClase(){
-    let siteNav = document.getElementById('sitenav');
-    siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menutoggle');
-    menuOpen.classList.toggle('menu-open');
-}
